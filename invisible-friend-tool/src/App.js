7acf7826cb906797c957f9948c90123c100ac529
JavierRibaldelRio <javierribal@gmail.com>
2022-01-04 14:36:31 +0100

    He preparado el app para programar

--- invisible-friend-tool/src/App.js
+++ invisible-friend-tool/src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import PreguntarNombres from './PreguntarNombres';
+
+import React, { Component } from 'react';
 
 
+class App extends Component {
 
-class App extends React.Component() {
+  constructor(props) {
+
+    super(props);
+    this.state = {
+
+      participantes: [],  //alamcena todos los participantes en el amigo invisible
+    }
+  }
+
+  //Añade al arrayy de participantes el nuevo participante
+  anyadirParticipante(a) {
+
+    if (this.state.participantes.includes(a) === false) {
+
+      this.setState({ participantes: this.state.participantes.concat(a) }); //Anyade al estado la nueva persona
+
+    }
+    else {
+      alert('Este nombre ya esta en uso, por favor, introduzca uno diferente')
+    }
+
+
+  }
 
   render() {
+
     return (
       <div className="App">
-
+        <PreguntarNombres anyadir={this.anyadirParticipante.bind(this)} participantes={this.state.participantes} />
       </div>
-    )
-  };
+    );
+
+  }
 }
 
 export default App;

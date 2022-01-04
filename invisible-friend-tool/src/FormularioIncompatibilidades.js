12f17209cae74a533df32a3841375386f091106d
JavierRibaldelRio <javierribal@gmail.com>
2022-01-04 14:38:04 +0100

    He empezado a trabajar en la incopativilidad

---
+++ invisible-friend-tool/src/FormularioIncompatibilidades.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class FormularioIncompatilidades extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (<form>
+
+        </form>);
+    }
+}
+
+export default FormularioIncompatilidades;

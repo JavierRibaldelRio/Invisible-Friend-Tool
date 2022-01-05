dde7567fec8e6abae96d0c36d7d37a9cd2809677
JavierRibaldelRio <javierribal@gmail.com>
2022-01-05 09:55:06 +0100

    He hecho una función que reinicia el array

---
+++ invisible-friend-tool/src/ReiniciarTabla.js
@@ -0,0 +1,25 @@
+
+//Se ocupa de poner todas las casillas en false mnos las impresindibles
+
+function reiniciar(arr) {
+
+    for (var i = 1; i < arr.length; i++) {
+
+        for (var j = 1; j < arr[0].length; j++) {
+
+            if (j === i || arr[i][j] === true) {
+
+                arr[i][j] = true;
+
+            }
+
+            else {
+
+                arr[i][j] = false;
+            }
+
+        }
+    }
+}
+
+export default reiniciar

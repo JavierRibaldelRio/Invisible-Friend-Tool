b076b563139ad1da102cf0f31ee86fa4ccc87d55
JavierRibaldelRio <javierribal@gmail.com>
2022-08-13 20:38:27 +0200

    Sustitución clase React por componente función

--- invisible-friend-tool/src/components/LiParticipante.js
+++ invisible-friend-tool/src/components/LiParticipante.js
@@ -1,17 +1,12 @@
 //Crea un li del paricipante
-import React, { Component } from 'react';
 
-class LiParticipante extends Component {
-    constructor(props) {
-        super(props);
-    }
+function LiParticipante(props) {
 
-    //Devuelve el li
-    render() {
-        const participante = this.props.participante;
-        const id = 'LiParticipante' + participante;
-        return (<li key={participante} id=''>{participante}</li>);
-    }
+    const participante = props.participante;
+    const id = 'LiParticipante' + participante;
+    return (<li key={participante} id={id}>{participante}</li>);
 }
+
+
 
 export default LiParticipante;

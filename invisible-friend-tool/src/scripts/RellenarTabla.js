d7fe885eb369b83bfd9276c530f65156a8ef2d5c
JavierRibaldelRio <javierribal@gmail.com>
2022-08-14 18:10:04 +0200

    Optimización función rellenar tabla

--- invisible-friend-tool/src/scripts/RellenarTabla.js
+++ invisible-friend-tool/src/scripts/RellenarTabla.js
@@ -1,27 +1,19 @@
-
 //Se ocupa de poner todas las casillas en false mnos las impresindibles
 
+function rellenarTabla(array) {
 
-
-
-function rellenarTabla(arr) {
+    var arr = [...array];
 
     for (var i = 1; i < arr.length; i++) {
-
         for (var j = 1; j < arr[0].length; j++) {
-
             if (j === i || arr[i][j] === true) {
-
                 arr[i][j] = true;
-
             }
-
             else {
-
                 arr[i][j] = false;
             }
-
         }
     }
+    return arr;
 }
 export default rellenarTabla;
